302dc33f1465b085eb1761d11d35e30676807e05
ballursajeed <ballursajeed@gmail.com>
2024-09-13 19:45:27 +0530

    cors middleware

--- api/src/index.js
+++ api/src/index.js
@@ -11,6 +11,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static('public'))
 app.use(cookieParser())
