7e7d552e385afa978a41c3cd09280d2ce0b97fe9
ballursajeed <ballursajeed@gmail.com>
2024-09-18 20:14:36 +0530

    Improved The UI of the Comment Section of the blgo

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,46 +1,69 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { SERVER } from '../constants/constants'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { SERVER } from '../constants/constants';
 import Avatar from './Avatar';
-import "../styles/Comment.css"
+import "../styles/Comment.css";
 
-const Comments = ({blog}) => {
-
-    const [comments,setComments] = useState([]);
-
+const Comments = ({ blog, close }) => {
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
 
     useEffect(() => {
-        const getAllComments = async() => {
-            const res = await axios.get(`${SERVER}/blog/getBlogComments/${blog?._id}`,{
+        const getAllComments = async () => {
+            const res = await axios.get(`${SERVER}/blog/getBlogComments/${blog?._id}`, {
                 withCredentials: true
             });
             if (res.data?.status === 200) {
-            setComments(res.data?.comments)    
+                setComments(res.data?.comments);
             }
-            
+        };
+        getAllComments();
+    }, [blog]);
+
+    const postComment = async () => {
+        if (!comment.trim()) return; // Prevent posting empty comments
+        const res = await axios.post(`${SERVER}/blog/comment/${blog?._id}`, {
+            content: comment
+        }, {
+            withCredentials: true
+        });
+
+        if (res.data?.status === 201) {
+            setComments([...comments, res.data?.comment]);
+            setComment(''); // Clear the input field after posting
         }
-      getAllComments()
-    },[])
+    };
 
-    console.log(comments[0]?.userDetails[0]?.username);
-    
-
-  return (
-    <div>
-     {
-        comments.map((comment) => 
-        {
-            return ( <> <div className='commentContainer'>
-                   <Avatar key={comment._id} user = {comment.userDetails}/> 
-                   <div>{comment?.content}</div>
+    return (
+        <div className='commentContainer'>
+            <div className='commentHeader'>
+                <h3>Comments: {comments.length}</h3>
+                <button className='closeButton' onClick={close}>X</button>
             </div>
-                      </> 
-            )
-        }   
-        )
-      }
-    </div>
-  )
-}
 
-export default Comments
+            <div className='commentsList'>
+                {comments.map((comment) => (
+                    <div className='commentItem' key={comment._id}>
+                        <Avatar user={comment.userDetails} />
+                        <div className='commentContent'>
+                            <p>{comment?.content}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className='commentInputContainer'>
+                <input 
+                    type='text'
+                    className='commentInput'
+                    placeholder='Add a comment...'
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)} 
+                />
+                <button className='submitButton' onClick={postComment}>Submit</button>
+            </div>
+        </div>
+    );
+};
+
+export default Comments;
