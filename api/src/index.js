f37d4fd016645f4820498f1643ea0db9de0fa0b4
ballursajeed <ballursajeed@gmail.com>
2024-09-20 18:19:02 +0530

    Deployment setup

--- api/src/index.js
+++ api/src/index.js
@@ -22,6 +22,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static('public'))
 app.use(cookieParser())
 
+app.get("/",(req,res) => {
+    res.send("Deployed testing")
+})
+
 //importing user rotuers
 import userRouter from "./routes/user.routes.js";
 //importing blog router
