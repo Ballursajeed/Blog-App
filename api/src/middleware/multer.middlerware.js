e3022ac37162fc194552f8e98790a8430d1daf97
ballursajeed <ballursajeed@gmail.com>
2024-09-20 20:27:54 +0530

    fixing Deployment error

--- api/src/middleware/multer.middlerware.js
+++ api/src/middleware/multer.middlerware.js
@@ -1,8 +1,12 @@
 import multer from "multer";
+import path from "path";
+
+// Define the root directory (assuming you are starting the app from "src")
+const rootDir = path.resolve();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/temp')
+      cb(null, path.join(rootDir, './public/temp'));
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname )
